8cc2e97843325ada50cbbb158c16a38b96a0b43e
Sean <89918592+rivezeri@users.noreply.github.com>
2022-06-02 16:51:14 -0500

    Add files via upload

--- nameandshame.py
+++ nameandshame.py
@@ -1,11 +1,13 @@
 import os 
 import random
+import time
 import discord
+from dotenv import load_dotenv
 from discord.ext import commands
 from keepalive import keepAlive
 from discord import Spotify
-import time
 
+load_dotenv()
 intents = discord.Intents.all()
 intents.members = True
 
@@ -17,7 +19,6 @@ client = commands.Bot(command_prefix='!', intents=intents)
 async def on_ready():
     print("Ready!")
     print(client.guilds)
-    # await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{len(client.guilds)}"))
     members = 0
     
     for guild in client.guilds:
@@ -29,16 +30,23 @@ async def on_ready():
 
 @client.event
 async def on_member_update(before, after):
-    # await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=random.choice(status)))
-    channel = client.get_channel()
+    
+    with open("defaultChannel.txt") as f:
+        find = f.readline()
+        channel = client.get_channel(find)
 
-    leagueMsgs = [ f"HEY {after.mention}, NICE GAME YOU'RE PLAYING THERE, LEAGUE. ",
+    # channel = client.get_channel()
+
+    leagueMsgs = [
+    
+    f"HEY {after.mention}, NICE GAME YOU'RE PLAYING THERE, LEAGUE. ",
     f"**LEAGUE ALERT**: {after.mention}",
     f"**ATTENTION CLIQUE**: {after.mention} IS CURRENTLY PLAYING LEAGUE OF LEGENDS. THAT IS ALL.",
     f"LEAGUE???? {after.mention} YOU COULD PLAY ANY GAME BUT YOU CHOOSE LEAGUE. GET EM BOYS",
     f"{after.mention} <- STUPID",
     f"{after.mention} NICE GAME IDIOT",
     f"YOU HAVE TEN SECONDS TO CLOSE LEAGUE OF LEGENDS OR YOU WILL BE BANNED. THE COUNTDOWN BEGINS NOW. {after.mention}."
+    
     ]
     
     # small head mode
@@ -46,33 +54,74 @@ async def on_member_update(before, after):
         print(after.activities)
 
         if str(after.activities) == '(<Game name=\'League of Legends\'>,)':
+            
             print("BIG INFRACTION")
-            try:
-                with open("hall-of-shame.txt", "a+") as f:
-                    f.write(after.name)
 
-                await channel.send(random.choice(leagueMsgs))
+            with open("hall-of-shame.txt", "a+") as f:
+                f.write(after.name)
 
-            except discord.errors.Forbidden:
-                print("Not valid permissions")
-                after.send("")
+            await channel.send(random.choice(leagueMsgs))
 
     for activity in after.activities:
 
-        if isinstance(activity, Spotify) and after.id == '' and activity.artist == 'Caravan Palace':
-            messages = [ f"ATTENTION : **{after.mention}** IS CURRENTLY LISTENING TO {activity.artist}. MAKE FUN OF {after.mention}",
+        if isinstance(activity, Spotify) and after.id == '138214725715623936' and activity.artist == 'Caravan Palace':
+            
+            print("INFRACTION")
+
+            messages = [
+                f"ATTENTION : **{after.mention}** IS CURRENTLY LISTENING TO {activity.artist}. MAKE FUN OF {after.mention}",
                 f"HEY YOU ABSOLUTE BAFFOON **{after.mention}**, STOP LISTENING TO {activity.artist}",
                 f"PLAY A BETTER SONG {after.mention}, {activity.artist.upper()}",
                 f"ATTENTION CLIQUE. {after.mention} IS NOW LISTENING TO CARAVAN PALACE. HAVE A NICE DAY."
             ]
 
-            print("INFRACTION")
-            with open("mcjobCounter.txt", "a+") as f:
-                f.write(1)
-            
             await channel.send(random.choice(messages))
+
+@client.event
+async def on_message(message): # exploitable function
+    if message.author == client.user:
+        return
+    
+    if message.content.startswith('/addMcJob'):
+        await message.channel.send('Added McJob to waiting folder. Expect approval shortly.')
+    
+    if message.content.startswith('/forceAddMcJob'): # admin only
+        # if message.author != message.author.guild_permissions.administrator:
+        #     return
+        
+        await message.channel.send('Added McJob')
+    
+    if message.content.startswith('/exploitable'):
+        
+        await message.channel.send('Sent!')
+    
+    if message.content.startswith('/redditMode'):
+
+        with open("redditMode.txt") as f:
+            find = f.readline()
+
+            if find == 'On':
+                f.write('Off')
+                await message.channel.send('Reddit mode has been *disabled*.')
+            else:
+                f.write('On')
+                await message.channel.send('Reddit mode has been *enabled*.')
+
+    if message.content.startswith('/setmain'):
+
+        with open("defaultChannel.txt", "w") as f:
+            f.write(str(message.channel.id))
+
+        await message.channel.send('Broadcasts will now be sent in this channel.')
+    
+    if message.id == '138214725715623936':
+        with open("redditMode.txt") as f:
+            find = f.readline()
+            if find == 'On':
+                await message.add_reaction(':downvote:')
+                await message.add_reaction(':upvote:')
 
 
 
 keepAlive()
-client.run(os.environ['TOKEN']) 
+client.run(os.getenv('TOKEN')) 
