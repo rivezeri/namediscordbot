76f6edc79b7002f127876c0a25e80e724622be1a
Sean <89918592+rivezeri@users.noreply.github.com>
2022-06-02 20:36:40 -0500

    Add files via upload

--- nameandshame.py
+++ nameandshame.py
@@ -2,18 +2,18 @@ import os
 import random
 import time
 import discord
+import uuid
+import requests
+import shutil
 from dotenv import load_dotenv
 from discord.ext import commands
 from keepalive import keepAlive
 from discord import Spotify
-import uuid
-import requests
-import shutil
+
 
 load_dotenv()
 intents = discord.Intents.all()
 intents.members = True
-
 
 client = commands.Bot(command_prefix='/', intents=intents)
 
@@ -35,10 +35,9 @@ async def on_ready():
 async def on_member_update(before, after):
     
     with open("defaultChannel.txt") as f:
+
         find = f.readline()
         channel = client.get_channel(find)
-
-    # channel = client.get_channel()
 
     leagueMsgs = [
     
@@ -82,30 +81,31 @@ async def on_member_update(before, after):
 
 @client.event
 async def on_message(message): # exploitable function
-    print(message.content)
+
     if message.author == client.user:
         return
 
     # enables / disables reddit mode
     if message.content.startswith('/reddit'):
         if message.author.id == '138214725715623936':
+
             await message.channel.send("""Sorry. You don\'t have permission to edit this mode.
             \nPlease say \"The dollar delicacy is real\" to disable.'""")
+            
             return
-        
 
         with open("redditMode.txt", "r+") as f:
+            
             find = f.read()
 
             if find == 'On':
-
                 f.seek(0)
                 f.truncate(0)
                 f.write('Off')
                 
                 await message.channel.send('Reddit mode has been *disabled*.')
-            else:
 
+            else:
                 f.seek(0)
                 f.truncate(0)
                 f.write('On')
@@ -121,33 +121,45 @@ async def on_message(message): # exploitable function
 
     # checks if reddit mode is enabled, if enabled will automatically add upvote/downvote functionality
     if str(message.author.id) == '138214725715623936' and message.channel.id != "849730083434135614":
+
         with open("redditMode.txt") as f:
+
             find = f.read()
             print(find == 'On')
+
             if find == 'On':
+
                 await message.add_reaction('<:upvote:385300941118898176>')
                 await message.add_reaction('<:downvote:385300951139090434>')
     
     if message.content.startswith('/addMcJob'):
+
         rand = random.randint(1,1000000)
         if message.attachments:
+
             for attach in message.attachments:
+
                 await attach.save(f"/home/rive/bot/exploitables/tba/{str(rand)} {attach.filename}")
+
             await message.channel.send('Your exploitable has been added.')
+            
         else:
             await message.channel.send('Please add an attachment.')
     
     if message.content.startswith('/exploitable'):
+
         dirListing = os.listdir('/home/rive/bot/exploitables/tba/')
-        linearShits = []
+        linear = []
 
         for item in dirListing:
-            linearShits.append(item)
+            linear.append(item)
     
-        with open(str('/home/rive/bot/exploitables/tba/' + random.choice(linearShits)),'rb') as f:
+        with open(str('/home/rive/bot/exploitables/tba/' + random.choice(linear)),'rb') as f:
+
             picture = discord.File(f)
             await message.channel.send(file=picture)
+            
 
 
 keepAlive()
-client.run(os.getenv('TOKEN')) 
+client.run(os.getenv('TOKEN'))
